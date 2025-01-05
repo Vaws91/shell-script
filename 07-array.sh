5f9dc32eb70d33296d75d87d0238d2d531558004
vidyasaranyap <saranya.phvlnv.com>
2025-01-05 23:32:26 +0530

    shell script

--- 07-array.sh
+++ 07-array.sh
@@ -3,7 +3,7 @@
 CRICKETER=("sachin" "dravid" "laxman")
 
 echo "first player: ${CRICKETER[0]}"
-echo "first player: ${CRICKETER[1]}"
-echo "first player: ${CRICKETER[2]}"
+echo "second player: ${CRICKETER[1]}"
+echo "third player: ${CRICKETER[2]}"
 
 echo "All players are: ${CRICKETER[@]}"
