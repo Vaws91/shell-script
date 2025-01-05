4602874f4175e13457a3425feb497a2c81e971df
vidyasaranyap <saranya.phvlnv.com>
2025-01-05 23:30:15 +0530

    shell script

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CRICKETER = ("sachin" "dravid" "laxman")
+
+echo "first player: ${CRICKETER[0]}"
+echo "first player: ${CRICKETER[1]}"
+echo "first player: ${CRICKETER[2]}"
+
+echo "All players are: ${CRICKETER[@]}"
