491e5667785782e1f6bec20b1ff1e5db46e8e959
vidyasaranyap <saranya.phvlnv.com>
2025-01-05 22:16:02 +0530

    Conversation modified

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PERSON1=Rajesh
-PERSON2=Sagar
 
-echo "$PERSON1: Hi $PERSON2"
-echo "$PERSON2: Hello $PERSON1"
-echo "$PERSON1: How are you doing?"
-echo "$PERSON2: I am fine. How about you?"
+echo "Vikram: Hi Amit"
+echo "Amit: Hello Vikram"
+echo "Vikram: How are you doing?"
+echo "Amit: I am fine. How about you?"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rajesh
+PERSON2=Sagar
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you doing?"
+echo "$PERSON2: I am fine. How about you?"
